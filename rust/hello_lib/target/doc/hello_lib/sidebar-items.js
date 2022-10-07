39a248487c5fe6e41ac2cc4f588594002eb4318c
David Barringer <nukdcbear@gmail.com>
2022-10-07 16:32:38 -0400

    Learning Rust by example

---
+++ rust/hello_lib/target/doc/hello_lib/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["hello","This function returns the greeting; Hello, world!"]]};
---
+++ rust/hello_lib/target/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"hello_lib":{"doc":"A Simple Hello World Crate","t":[5],"n":["hello"],"q":["hello_lib"],"d":["This function returns the greeting; Hello, world!"],"i":[0],"f":[[[],1]],"p":[[3,"String"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
